9336fc663c33b0a12e6352af7bc32ab2aed79714
Donald Vallejo <donny.vallejo@gmail.com>
2019-09-17 15:51:40 -0700

    added route test, possible fix for displaying gifs

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from random import sample
 
 """ Settings for server, api and pretty print """
 app = Flask(__name__)
-pp = pprint.PrettyPrinter(indent=4)
+pp = pprint.PrettyPrinter(indent=1)
 params = {
     "q" : "Im not sure how this built-in works??",
     "apikey" : "6KSAHML0KPXD",
@@ -19,75 +19,48 @@ params = {
 def index():
     """Show the homepage and ask the user's name."""
     return render_template('index.html', gif = gifSearch())
-    
 
 """ Main function for the Gif search """
 def gifSearch():
     search = request.args.get('search')
     r = requests.get(
         "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search, params['apikey'], params['lmt']))
+    pp.pprint(['results'])
     gif = r.json()['results']
-    return gif[9]
+    return gif[2]
     
+    """ Loop for printed Gifs """
+    # gifList = []
+    # gif = r.json()
+    # print(type(gif))
+    # resultsList = gif['results']
+    # for gifs in resultsList:
+    #     medialist = gifs['media']
+    #     for medias in medialist:
+    #         gifList.append(medias['gif']['url'])
+    #         pp.pprint(medias['gif']['url'])
+
+    # return gifList
+    
+    # """ Route Test's """
+    # class AppTests(unittest.TestCase): 
+
+    # def setUp(self):
+    #     self.app = app.test_client()
+    #     self.app.testing = True 
+
+    # def test_home_status_code(self):
+    #     result = self.app.get('/') 
+
+    #     self.assertEqual(result.status_code, 200)
+    #     self.assertEqual(result.status_code, 500)
+    #     self.assertEqual(result.status_code, 404)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-"""Return homepage."""
-    # TODO: Extract the query term from url using request.args.get()
-
-    # TODO: Make 'params' dictionary containing:
-    # a) the query term, 'q'
-    # b) your API key, 'key'
-    # c) how many GIFs to return, 'limit'
-
-    # TODO: Make an API call to Tenor using the 'requests' library. For 
-    # reference on how to use Tenor, see: 
-    # https://tenor.com/gifapi/documentation
-
-    # TODO: Use the '.json()' function to get the JSON of the returned response
-    # object
 
     # TODO: Using dictionary notation, get the 'results' field of the JSON,
     # which contains the GIFs as a list
 
     # TODO: Render the 'index.html' template, passing the list of gifs as a
     # named parameter called 'gifs'
-
-
-"""
-# our test search
-search_term = "love"
-
-# get the top 8 GIFs for the search term
-r = requests.get(
-    "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s&anon_id=%s" %   
-     (search_term, apikey, lmt, anon_id))
-
-if r.status_code == 200:
-    # load the GIFs using the urls for the smaller GIF sizes
-    pp = pprint.PrettyPrinter(indent=4)
-    top_8gifs = json.loads(r.content)
-    pp.pprint(top_8gifs) #pretty prints the json file.
-    for i in range(len(top_8gifs['results'])):
-        url = top_8gifs['results'][i]['media'][0]['gif']['url'] #This is the url from json.
-        print (url)
-        urllib.request.urlretrieve(url, str(i)+'.gif') #Downloads the gif file.
-else:
-    top_8gifs = None
-
-@app.route('/gifSearch')
-def get_gif():
-
-    name = request.args.get('name')
-    show_gifs = request.args.get('show_gifs')
-    print('show_gifs:', show_gifs)
-    gifs_to_show = sample(gifs)
-
-    return render_template(
-        'gifs.html', 
-        name=name, 
-        show_gifs=show_gifs, 
-        gifs=gifs_to_show)   
-"""
